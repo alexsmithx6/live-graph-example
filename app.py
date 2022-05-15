21c2af847d9524ef6ffe132e2ba47a7b7529ad20
Alexander Smith <alexsmithx6@gmail.com>
2022-05-16 02:58:58 +1000

    example works with live data feed

---
+++ app.py
@@ -0,0 +1,27 @@
+from flask import Flask, render_template, jsonify, url_for
+
+import numpy as np
+import pandas as pd
+
+N = 300
+
+# Flask application:
+app = Flask(__name__)
+app.config['SECRET_KEY'] = '123'
+app.config['TEMPLATES_AUTO_RELOAD'] = True
+
+@app.route('/', methods=['GET', 'POST'])
+def index():
+    return render_template('graph.html', title='Live Data Feed Example')
+
+@app.route('/data.csv', methods=['GET', 'POST'])
+def renderData():
+    y1 = np.random.rand(N, 1).flatten()
+    y2 = np.random.rand(N, 1).flatten()
+    y3 = (0.5 + 0.5 * np.random.rand(N, 1)).flatten()
+    y4 = (0.5 + 0.5 * np.random.rand(N, 1)).flatten()
+    array = np.stack((y1, y2, y3, y4))
+    return jsonify(array.tolist())
+
+if __name__ == '__main__':
+    app.run(debug=True, host='0.0.0.0', use_reloader=False)
---
+++ static/live-graph.js
@@ -0,0 +1,63 @@
+// Create the chart
+const chart = Highcharts.chart('container', {
+    title: {
+        text: 'CSV Data'
+    },
+    exporting: {
+        enabled: true
+    },
+    yAxis: {
+        crosshair: {
+            label: {
+                enabled: true,
+                backgroundColor: 'green'
+
+            },
+            color: 'green'
+        }
+    },
+    chart: {
+        animation: {
+            duration: 1
+        }
+    },
+    series: [{
+        name: 'y1',
+        data: [],
+        visible: true,
+    }, {
+        name: 'y2',
+        data: [],
+        visible: true,
+    }, {
+        name: 'y3',
+        data: [],
+        visible: true,
+    }, {
+        name: 'y4',
+        data: [],
+        visible: true,
+    }]
+});
+
+setInterval(function () {
+
+    fetch('/data.csv').then((response) => {
+    
+        return response.json();
+    
+    }).then((data) => {
+
+        for (let ind = 0; ind < chart.series.length; ind++) {
+            chart.series[ind].setData(data[ind])
+        }
+        chart.redraw();
+
+    }).catch((err) => {
+
+        // Do something for an error here
+        console.log(err);
+
+    });
+
+}, 500);
